20f33d7a339ba2ea87e253be7676ad7ca12c488c
Bozorov Shohrux <94338979+bozorovshohrux5577@users.noreply.github.com>
2022-03-28 14:48:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,353 @@
+
+
+// bodyga ranglarni chiqarish uchun funksiya
+{
+    function a (){
+        const color = document.querySelectorAll(".color")[0].value;
+        document.body.style.backgroundColor = color;
+    }
+    function b(){
+        const color = document.querySelectorAll(".color")[1].value;
+        document.body.style.backgroundColor = color;
+    }
+    function d(){
+        const color = document.querySelectorAll(".color")[2].value;
+        document.body.style.backgroundColor = color;
+    }
+    function e(){
+        const color = document.querySelectorAll(".color")[3].value;
+        document.body.style.backgroundColor = color;
+    }
+    function f(){
+        const color = document.querySelectorAll(".color")[4].value;
+        document.body.style.backgroundColor = color;
+    }
+    function g(){
+        const color = document.querySelectorAll(".color")[5].value;
+        document.body.style.backgroundColor = color;
+    }
+    function h(){
+        const color = document.querySelectorAll(".color")[6].value;
+        document.body.style.backgroundColor = color;
+    }
+    function i(){
+        const color = document.querySelectorAll(".color")[7].value;
+        document.body.style.backgroundColor = color;
+    }
+    function j(){
+        const color = document.querySelectorAll(".color")[8].value;
+        document.body.style.backgroundColor = color;
+    }
+    function k(){
+        const color = document.querySelectorAll(".color")[9].value;
+        document.body.style.backgroundColor = color;
+    }
+    function l(){
+        const color = document.querySelectorAll(".color")[10].value;
+        document.body.style.backgroundColor = color;
+    }
+    function m(){
+        const color = document.querySelectorAll(".color")[11].value;
+        document.body.style.backgroundColor = color;
+    }
+    function n(){
+        const color = document.querySelectorAll(".color")[12].value;
+        document.body.style.backgroundColor = color;
+    }
+    function o(){
+        const color = document.querySelectorAll(".color")[13].value;
+        document.body.style.backgroundColor = color;
+    }
+    function p(){
+        const color = document.querySelectorAll(".color")[14].value;
+        document.body.style.backgroundColor = color;
+    }
+    function q(){
+        const color = document.querySelectorAll(".color")[15].value;
+        document.body.style.backgroundColor = color;
+    }
+    function r(){
+        const color = document.querySelectorAll(".color")[16].value;
+        document.body.style.backgroundColor = color;
+    }
+    function s(){
+        const color = document.querySelectorAll(".color")[17].value;
+        document.body.style.backgroundColor = color;
+    }
+    function t(){
+        const color = document.querySelectorAll(".color")[18].value;
+        document.body.style.backgroundColor = color;
+    }
+    function u(){
+        const color = document.querySelectorAll(".color")[19].value;
+        document.body.style.backgroundColor = color;
+    }
+    function v(){
+        const color = document.querySelectorAll(".color")[20].value;
+        document.body.style.backgroundColor = color;
+    }
+}
+
+
+// rangni kodini saqlash uchun funksiya
+
+{
+    function aa(){
+        const text = document.querySelectorAll(".vall")[0].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ab (){
+        const text = document.querySelectorAll(".vall")[1].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ad (){
+        const text = document.querySelectorAll(".vall")[2].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ae (){
+        const text = document.querySelectorAll(".vall")[3].value;
+        navigator.clipboard.writeText(text)
+    }
+    function af (){
+        const text = document.querySelectorAll(".vall")[4].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ag (){
+        const text = document.querySelectorAll(".vall")[5].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ah (){
+        const text = document.querySelectorAll(".vall")[6].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ai (){
+        const text = document.querySelectorAll(".vall")[7].value;
+        navigator.clipboard.writeText(text)
+    }
+    function aj (){
+        const text = document.querySelectorAll(".vall")[8].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ak (){
+        const text = document.querySelectorAll(".vall")[9].value;
+        navigator.clipboard.writeText(text)
+    }
+    function al (){
+        const text = document.querySelectorAll(".vall")[10].value;
+        navigator.clipboard.writeText(text)
+    }
+    function am (){
+        const text = document.querySelectorAll(".vall")[11].value;
+        navigator.clipboard.writeText(text)
+    }
+    function an (){
+        const text = document.querySelectorAll(".vall")[12].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ao (){
+        const text = document.querySelectorAll(".vall")[13].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ap (){
+        const text = document.querySelectorAll(".vall")[14].value;
+        navigator.clipboard.writeText(text)
+    }
+    function aq (){
+        const text = document.querySelectorAll(".vall")[15].value;
+        navigator.clipboard.writeText(text)
+    }
+    function ar (){
+        const text = document.querySelectorAll(".vall")[16].value;
+        navigator.clipboard.writeText(text)
+    }
+    function as (){
+        const text = document.querySelectorAll(".vall")[17].value;
+        navigator.clipboard.writeText(text)
+    }
+    function at (){
+        const text = document.querySelectorAll(".vall")[18].value;
+        navigator.clipboard.writeText(text)
+    }
+    function au (){
+        const text = document.querySelectorAll(".vall")[19].value;
+        navigator.clipboard.writeText(text)
+    }
+    function av (){
+        const text = document.querySelectorAll(".vall")[20].value;
+        navigator.clipboard.writeText(text)
+    }
+};
+
+
+{
+    var toastTrigger = document.getElementById('la')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lb')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('ld')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('le')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lf')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lg')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lh')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('li')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lj')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lk')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('ll')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lm')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('ln')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lo')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lp')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lq')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lr')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('ls')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lt')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lu')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+    var toastTrigger = document.getElementById('lv')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger){
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+
+}
